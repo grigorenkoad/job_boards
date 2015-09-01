e45841bac3e056d43f740bcf7d0dafa634c5a900
grigorenkoad <grigorenkoad@gmail.com>
2015-09-02 01:10:52 +0300

    merge pages, filter

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,2 @@
 class Job < ActiveRecord::Base
-  
-  scope :title, -> (title) { where title: title }
-  scope :location, -> (location) { where location: location }
-  scope :company, -> (company) { where company: company }
-  scope :category, -> (category) { where category: category }
-
 end
